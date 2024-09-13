8c09eb3996871be8941d439d4aba0469b1b0ab69
Kanishka Dutta <122324412+KDCodes23@users.noreply.github.com>
2024-09-13 14:15:26 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace _45Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_TLeftBox = new System.Windows.Forms.Button();
+            this.btn_TCenterBox = new System.Windows.Forms.Button();
+            this.btn_TRightBox = new System.Windows.Forms.Button();
+            this.btn_MLeftBox = new System.Windows.Forms.Button();
+            this.btn_MCenterBox = new System.Windows.Forms.Button();
+            this.btn_MRightBox = new System.Windows.Forms.Button();
+            this.btn_BLeftBox = new System.Windows.Forms.Button();
+            this.btn_BCenterBox = new System.Windows.Forms.Button();
+            this.btn_BRightBox = new System.Windows.Forms.Button();
+            this.gB_Buttons = new System.Windows.Forms.GroupBox();
+            this.gB_Buttons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_TLeftBox
+            // 
+            this.btn_TLeftBox.BackColor = System.Drawing.Color.Azure;
+            this.btn_TLeftBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_TLeftBox.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TLeftBox.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btn_TLeftBox.Location = new System.Drawing.Point(10, 13);
+            this.btn_TLeftBox.Name = "btn_TLeftBox";
+            this.btn_TLeftBox.Size = new System.Drawing.Size(86, 72);
+            this.btn_TLeftBox.TabIndex = 0;
+            this.btn_TLeftBox.Text = "X";
+            this.btn_TLeftBox.UseVisualStyleBackColor = false;
+            // 
+            // btn_TCenterBox
+            // 
+            this.btn_TCenterBox.BackColor = System.Drawing.Color.Azure;
+            this.btn_TCenterBox.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TCenterBox.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btn_TCenterBox.Location = new System.Drawing.Point(102, 13);
+            this.btn_TCenterBox.Name = "btn_TCenterBox";
+            this.btn_TCenterBox.Size = new System.Drawing.Size(86, 72);
+            this.btn_TCenterBox.TabIndex = 1;
+            this.btn_TCenterBox.Text = "X";
+            this.btn_TCenterBox.UseVisualStyleBackColor = false;
+            // 
+            // btn_TRightBox
+            // 
+            this.btn_TRightBox.BackColor = System.Drawing.Color.Azure;
+            this.btn_TRightBox.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TRightBox.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btn_TRightBox.Location = new System.Drawing.Point(194, 13);
+            this.btn_TRightBox.Name = "btn_TRightBox";
+            this.btn_TRightBox.Size = new System.Drawing.Size(86, 72);
+            this.btn_TRightBox.TabIndex = 2;
+            this.btn_TRightBox.Text = "X";
+            this.btn_TRightBox.UseVisualStyleBackColor = false;
+            // 
+            // btn_MLeftBox
+            // 
+            this.btn_MLeftBox.BackColor = System.Drawing.Color.Azure;
+            this.btn_MLeftBox.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_MLeftBox.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btn_MLeftBox.Location = new System.Drawing.Point(10, 91);
+            this.btn_MLeftBox.Name = "btn_MLeftBox";
+            this.btn_MLeftBox.Size = new System.Drawing.Size(86, 72);
+            this.btn_MLeftBox.TabIndex = 3;
+            this.btn_MLeftBox.Text = "X";
+            this.btn_MLeftBox.UseVisualStyleBackColor = false;
+            // 
+            // btn_MCenterBox
+            // 
+            this.btn_MCenterBox.BackColor = System.Drawing.Color.Azure;
+            this.btn_MCenterBox.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_MCenterBox.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btn_MCenterBox.Location = new System.Drawing.Point(102, 91);
+            this.btn_MCenterBox.Name = "btn_MCenterBox";
+            this.btn_MCenterBox.Size = new System.Drawing.Size(86, 72);
+            this.btn_MCenterBox.TabIndex = 4;
+            this.btn_MCenterBox.Text = "X";
+            this.btn_MCenterBox.UseVisualStyleBackColor = false;
+            // 
+            // btn_MRightBox
+            // 
+            this.btn_MRightBox.BackColor = System.Drawing.Color.Azure;
+            this.btn_MRightBox.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_MRightBox.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btn_MRightBox.Location = new System.Drawing.Point(194, 91);
+            this.btn_MRightBox.Name = "btn_MRightBox";
+            this.btn_MRightBox.Size = new System.Drawing.Size(86, 72);
+            this.btn_MRightBox.TabIndex = 5;
+            this.btn_MRightBox.Text = "X";
+            this.btn_MRightBox.UseVisualStyleBackColor = false;
+            // 
+            // btn_BLeftBox
+            // 
+            this.btn_BLeftBox.BackColor = System.Drawing.Color.Azure;
+            this.btn_BLeftBox.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_BLeftBox.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btn_BLeftBox.Location = new System.Drawing.Point(10, 169);
+            this.btn_BLeftBox.Name = "btn_BLeftBox";
+            this.btn_BLeftBox.Size = new System.Drawing.Size(86, 72);
+            this.btn_BLeftBox.TabIndex = 6;
+            this.btn_BLeftBox.Text = "X";
+            this.btn_BLeftBox.UseVisualStyleBackColor = false;
+            // 
+            // btn_BCenterBox
+            // 
+            this.btn_BCenterBox.BackColor = System.Drawing.Color.Azure;
+            this.btn_BCenterBox.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_BCenterBox.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btn_BCenterBox.Location = new System.Drawing.Point(102, 169);
+            this.btn_BCenterBox.Name = "btn_BCenterBox";
+            this.btn_BCenterBox.Size = new System.Drawing.Size(86, 72);
+            this.btn_BCenterBox.TabIndex = 7;
+            this.btn_BCenterBox.Text = "X";
+            this.btn_BCenterBox.UseVisualStyleBackColor = false;
+            // 
+            // btn_BRightBox
+            // 
+            this.btn_BRightBox.BackColor = System.Drawing.Color.Azure;
+            this.btn_BRightBox.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_BRightBox.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btn_BRightBox.Location = new System.Drawing.Point(194, 169);
+            this.btn_BRightBox.Name = "btn_BRightBox";
+            this.btn_BRightBox.Size = new System.Drawing.Size(86, 72);
+            this.btn_BRightBox.TabIndex = 8;
+            this.btn_BRightBox.Text = "X";
+            this.btn_BRightBox.UseVisualStyleBackColor = false;
+            // 
+            // gB_Buttons
+            // 
+            this.gB_Buttons.Controls.Add(this.btn_MRightBox);
+            this.gB_Buttons.Controls.Add(this.btn_BRightBox);
+            this.gB_Buttons.Controls.Add(this.btn_TLeftBox);
+            this.gB_Buttons.Controls.Add(this.btn_BCenterBox);
+            this.gB_Buttons.Controls.Add(this.btn_TCenterBox);
+            this.gB_Buttons.Controls.Add(this.btn_BLeftBox);
+            this.gB_Buttons.Controls.Add(this.btn_TRightBox);
+            this.gB_Buttons.Controls.Add(this.btn_MLeftBox);
+            this.gB_Buttons.Controls.Add(this.btn_MCenterBox);
+            this.gB_Buttons.Location = new System.Drawing.Point(25, 32);
+            this.gB_Buttons.Name = "gB_Buttons";
+            this.gB_Buttons.Size = new System.Drawing.Size(293, 252);
+            this.gB_Buttons.TabIndex = 9;
+            this.gB_Buttons.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkViolet;
+            this.ClientSize = new System.Drawing.Size(785, 504);
+            this.Controls.Add(this.gB_Buttons);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TicTackToe Game";
+            this.gB_Buttons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_TLeftBox;
+        private System.Windows.Forms.Button btn_TCenterBox;
+        private System.Windows.Forms.Button btn_TRightBox;
+        private System.Windows.Forms.Button btn_MLeftBox;
+        private System.Windows.Forms.Button btn_MCenterBox;
+        private System.Windows.Forms.Button btn_MRightBox;
+        private System.Windows.Forms.Button btn_BLeftBox;
+        private System.Windows.Forms.Button btn_BCenterBox;
+        private System.Windows.Forms.Button btn_BRightBox;
+        private System.Windows.Forms.GroupBox gB_Buttons;
+    }
+}
+
